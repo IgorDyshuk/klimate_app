6f9a0f3299cf2bbd8c6bc072a7c8a3db79aabc12
IgorDyshuk <igordysuk@gmail.com>
2025-04-04 14:27:17 +0300

    Add React Query and Configure API

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,30 @@ import './App.css'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "@/components/ui/layout.tsx";
 import {ThemeProvider} from "@/contex/theme-proider.tsx";
-import WatherDashbord from "@/pages/wather-dashbord.tsx";
+import WeatherDashboard from "@/pages/wather-dashbord.tsx";
 import CityPage from "@/pages/CityPage.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
 
 function App() {
 
     return (
         <>
-            <BrowserRouter>
-                <ThemeProvider defaultTheme="dark">
-                    <Layout>
-                        <Routes>
-                            <Route path="/" element={<WatherDashbord/>} />
-                            <Route path="/city/:cityName" element={<CityPage/>} />
-                        </Routes>
-                    </Layout>
-                </ThemeProvider>
-            </BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
+                    <ThemeProvider defaultTheme="dark">
+                        <Layout>
+                            <Routes>
+                                <Route path="/" element={<WeatherDashboard/>}/>
+                                <Route path="/city/:cityName" element={<CityPage/>}/>
+                            </Routes>
+                        </Layout>
+                    </ThemeProvider>
+                </BrowserRouter>
+                <ReactQueryDevtools initialIsOpen={false}/>
+            </QueryClientProvider>
         </>
     )
 }
--- src/pages/wather-dashbord.tsx
+++ src/pages/wather-dashbord.tsx
@@ -1,4 +1,4 @@
-export default function WatherDashbord() {
+export default function WeatherDashboard() {
     return (
         <>
             Wather Dashbord
