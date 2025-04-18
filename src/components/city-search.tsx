cc8ee89cab9fde2e4a082834ecf9a75042b4f516
IgorDyshuk <igordysuk@gmail.com>
2025-04-18 13:21:16 +0300

    Fix: Reset Query on Dialog open

--- src/components/city-search.tsx
+++ src/components/city-search.tsx
@@ -47,7 +47,12 @@ export default function CitySearch() {
             <Button
                 variant="outline"
                 className={"relative w-full justify-start text-sm text-muted-foreground sm:pr-12 md:w-40 lg:w-64"}
-                onClick={() => setOpen(true)}
+                onClick={
+                    () => {
+                        setQuery('')
+                        setOpen(true)
+                    }
+                }
             >
                 <Search className="mr-2 h-4   w-4"/>
                 Search Cities...
